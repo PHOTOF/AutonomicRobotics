05bf92c6c84a173654bc51effc7b0b8a871b3799
yoavhenig <33389702+yoavhenig@users.noreply.github.com>
2019-05-27 13:21:30 +0300

    add plot graph

--- BeresheetSimulator/beresheet.py
+++ BeresheetSimulator/beresheet.py
@@ -1,26 +1,27 @@
 import math
+import matplotlib.pyplot as plt
 
-RADIUS = 3475 * 1000; # meters
-ACC = 1.622; # m / s ^ 2
-EQ_SPEED = 1700; # m / s
-
-def getAcc(speed):
-    n = abs(speed) / EQ_SPEED;
-    ans = (1 - n) * ACC;
-    return ans;
-
+# Moon variables
+MOON_RADIUS = 3475 * 1000  # meters
+MOON_ACC = 1.622  # m / s ^ 2
+EQ_SPEED = 1700  # m / s
 WEIGHT_EMP = 165  # kg
 WEIGHT_FULE = 420  # kg
 WEIGHT_FULL = WEIGHT_EMP + WEIGHT_FULE  # kg
-MAIN_ENG_F = 430 # N
+MAIN_ENG_F = 430  # N
 SECOND_ENG_F = 25  # N
 MAIN_BURN = 0.15  # liter per sec, 12 liter per m'
 SECOND_BURN = 0.009  # liter per sec 0.6 liter per m'
 ALL_BURN = MAIN_BURN + 8 * SECOND_BURN
 
+def get_acc(speed):
+    n = abs(speed) / EQ_SPEED
+    ans = (1 - n) * MOON_ACC
+    return ans
 
-def accMax(weight):
+def acc_max(weight):
     return acc(weight, True, 8)
+
 
 def acc(weight, main, seconds):
     t = 0
@@ -30,6 +31,10 @@ def acc(weight, main, seconds):
     ans = t / weight
     return ans
 
+
+x = []
+y = []
+z = []
 
 # starting point:
 verticalSpeed = 24.8
@@ -43,9 +48,14 @@ fuel = 121  #
 weight = WEIGHT_EMP + fuel
 print("time, vertical speed, horizontal speed, altitude, angle, weight, acc")
 NN = 0.7  # rate[0,1]
-
+dist = 1
 # ***** main simulation loop ******
 while alt > 0:
+    # make up some data
+    x.append(0 - dist)
+    y.append(alt)
+    z.append(verticalSpeed)
+
     if time % 10 == 0 or alt < 100:
         print("%s, %s, %s, %s, %s, %s, %s" % (time, verticalSpeed, horizontalSpeed, alt, ang, weight, acceleration))
 
@@ -53,6 +63,7 @@ while alt > 0:
     if alt > 2000:  # maintain a vertical speed of [20-25] m/s
         if verticalSpeed > 25:
             NN += 0.003 * dt  # more power for braking
+            # NN = 1  # maximum braking!
         if verticalSpeed < 20:
             NN -= 0.003 * dt  # less power for braking
     # lower than 2 km - horizontal speed should be close to zero
@@ -60,12 +71,13 @@ while alt > 0:
         if ang > 3:
             ang -= 3  # rotate to vertical position.
         else:
-            ang =0
+            ang = 0
         NN = 0.5  # brake slowly, a proper PID controller here is needed!
         if horizontalSpeed < 2:
             horizontalSpeed = 0
         if alt < 125:  # very close to the ground!
             NN = 1  # maximum braking!
+            # NN += 0.003 * dt  # more power for braking
             if verticalSpeed < 5:
                 NN = 0.7  # if it is slow enough - go easy on the brakes
 
@@ -76,13 +88,13 @@ while alt > 0:
     ang_rad = math.radians(ang)
     h_acc = math.sin(ang_rad) * acceleration
     v_acc = math.cos(ang_rad) * acceleration
-    vacc = getAcc(horizontalSpeed)
+    vacc = get_acc(horizontalSpeed)
     time += dt
     dw = dt * ALL_BURN * NN
     if fuel > 0:
         fuel -= dw
         weight = WEIGHT_EMP + fuel
-        acceleration = NN * accMax(weight)
+        acceleration = NN * acc_max(weight)
 
     else:  # ran out of fuel
         acceleration = 0
@@ -92,6 +104,23 @@ while alt > 0:
         horizontalSpeed -= h_acc * dt
     verticalSpeed -= v_acc * dt
     alt -= dt * verticalSpeed
+    dist -= horizontalSpeed*dt
+
+
+# plot
+plt.plot(x, y, label = "Altitude")
+plt.plot(x, z, label = "Vertical Speed")
 
 
 
+# naming the garph
+plt.title(' Beresheet Landing: (time - sec) ' + str((time/60)))
+# naming the x axis
+plt.xlabel('Dist')
+# naming the y axis
+plt.ylabel('Altitude')
+
+# beautify the x-labels
+# plt.gcf().autofmt_xdate()
+
+plt.show()
